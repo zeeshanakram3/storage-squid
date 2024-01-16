74f77c8dd2950451963f6b268921a714cb4dbf33
Zeeshan Akram <97m.zeeshan@gmail.com>
2024-01-16 17:48:17 +0500

    fix query to return up to date package version each time

--- db/migrations/2200000000000-Operator.js
+++ db/migrations/2200000000000-Operator.js
@@ -1,24 +1,7 @@
-const fs = require('fs')
-const path = require('path')
-
 module.exports = class Operator2200000000000 {
   name = 'Operator2200000000000'
 
   async up(db) {
-    // Path to your package.json file
-    const packageJsonPath = path.join(__dirname, '../../package.json')
-
-    // Read the package.json file synchronously
-    const data = fs.readFileSync(packageJsonPath, 'utf8')
-
-    // Parse the JSON data
-    const packageJson = JSON.parse(data)
-
-    await db.query(
-      `INSERT INTO "squid_version" ("id", "version") 
-       VALUES ('${packageJson.name}' ,'${packageJson.version}');`
-    )
-
     // Create pg_stat_statements extension for analyzing query stats
     await db.query(`CREATE EXTENSION pg_stat_statements;`)
   }
---
+++ src/server-extension/resolvers/squidVersion.ts
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import _ from 'lodash'
+import pkgDir from 'pkg-dir'
+import 'reflect-metadata'
+import { Field, ObjectType, Query, Resolver } from 'type-graphql'
+
+const pkgVersion = (): Promise<string> => {
+  // Path to  package.json file
+  const packageJsonPath = `${pkgDir.sync()}/package.json`
+
+  // Read the package.json file synchronously
+  const data = fs.readFileSync(packageJsonPath, 'utf8')
+
+  // Parse the JSON data
+  const packageJson = JSON.parse(data)
+
+  return packageJson.version
+}
+
+const memoizedPkgVersion = _.memoize(pkgVersion)
+
+@ObjectType()
+export class SquidVersion {
+  @Field(() => String, { nullable: false })
+  version!: string
+}
+
+@Resolver()
+export class SquidVersionResolver {
+  private version: string
+
+  constructor() {}
+
+  @Query(() => SquidVersion)
+  async squidVersion(): Promise<SquidVersion> {
+    // Get package version
+    const version = await memoizedPkgVersion()
+
+    return { version }
+  }
+}
